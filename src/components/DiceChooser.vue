e599e8c79e3e3525b563207e7a86a7cdb2418cad
Rick Henry <rickhenry@rickhenry.dev>
2023-09-05 18:18:36 -0400

    add h1 tag for seo
    Signed-off-by: Rick Henry <rickhenry@rickhenry.dev>

--- src/components/DiceChooser.vue
+++ src/components/DiceChooser.vue
@@ -1,30 +1,54 @@
 <template>
   <RadioGroup v-model="selectedDie">
-    <RadioGroupLabel class="text-base font-medium text-gray-900 text-2xl">
+    <RadioGroupLabel class="font-medium text-gray-900 text-2xl">
       Which type of dice are you rolling?
     </RadioGroupLabel>
 
-    <div class="mt-4 grid grid-cols-1 xs:grid-cols-2 xs:gap-x-4 gap-y-6 sm:grid-cols-3">
-      <RadioGroupOption as="template"
-                        v-for="die in allDice"
-                        :key="die.id"
-                        :value="die"
-                        v-slot="{ checked, active }">
-        <div :class="[checked ? 'border-transparent' : 'border-gray-300', active ? 'ring-2 ring-emerald-500' : '', 'relative bg-white border rounded-lg shadow-sm p-4 flex cursor-pointer focus:outline-none']">
+    <div
+      class="mt-4 grid grid-cols-1 xs:grid-cols-2 xs:gap-x-4 gap-y-6 sm:grid-cols-3"
+    >
+      <RadioGroupOption
+        as="template"
+        v-for="die in allDice"
+        :key="die.id"
+        :value="die"
+        v-slot="{ checked, active }"
+      >
+        <div
+          :class="[
+            checked ? 'border-transparent' : 'border-gray-300',
+            active ? 'ring-2 ring-emerald-500' : '',
+            'relative bg-white border rounded-lg shadow-sm p-4 flex cursor-pointer focus:outline-none',
+          ]"
+        >
           <div class="flex-1 flex">
             <div class="flex flex-col w-full justify-center items-center">
-              <RadioGroupLabel as="span" class="block text-sm font-medium text-gray-900">
+              <RadioGroupLabel
+                as="span"
+                class="block text-sm font-medium text-gray-900"
+              >
                 {{ die.name }}
               </RadioGroupLabel>
-              <RadioGroupDescription as="div"
-                                     class="mt-1 w-full flex justify-center items-center text-sm text-gray-500">
+              <RadioGroupDescription
+                as="div"
+                class="mt-1 w-full flex justify-center items-center text-sm text-gray-500"
+              >
                 <component :is="`${die.icon}`"></component>
               </RadioGroupDescription>
             </div>
           </div>
-          <CheckCircleIcon :class="[!checked ? 'invisible' : '', 'h-5 w-5 text-emerald-600']" aria-hidden="true" />
-          <div :class="[active ? 'border' : 'border-2', checked ? 'border-emerald-500' : 'border-transparent', 'absolute -inset-px rounded-lg pointer-events-none']"
-               aria-hidden="true" />
+          <CheckCircleIcon
+            :class="[!checked ? 'invisible' : '', 'h-5 w-5 text-emerald-600']"
+            aria-hidden="true"
+          />
+          <div
+            :class="[
+              active ? 'border' : 'border-2',
+              checked ? 'border-emerald-500' : 'border-transparent',
+              'absolute -inset-px rounded-lg pointer-events-none',
+            ]"
+            aria-hidden="true"
+          />
         </div>
       </RadioGroupOption>
     </div>
@@ -32,7 +56,12 @@
 </template>
 
 <script lang="ts">
-import { RadioGroup, RadioGroupDescription, RadioGroupLabel, RadioGroupOption } from "@headlessui/vue";
+import {
+  RadioGroup,
+  RadioGroupDescription,
+  RadioGroupLabel,
+  RadioGroupOption,
+} from "@headlessui/vue";
 import { CheckCircleIcon } from "@heroicons/vue/solid/index.js";
 import D4Icon from "@/components/dice-icons/D4Icon.vue";
 import D6Icon from "@/components/dice-icons/D6Icon.vue";
@@ -41,7 +70,7 @@ import D10Icon from "@/components/dice-icons/D10Icon.vue";
 import D12Icon from "@/components/dice-icons/D12Icon.vue";
 import D20Icon from "@/components/dice-icons/D20Icon.vue";
 import useDice from "@/store/dice";
-import {defineComponent} from "vue";
+import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "DiceChooser",
@@ -62,7 +91,8 @@ export default defineComponent({
     const { allDice, selectedDie } = useDice();
 
     return {
-      allDice, selectedDie,
+      allDice,
+      selectedDie,
     };
   },
 });
--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,35 +1,45 @@
 <template>
   <div class="p-8 shadow print:shadow-none">
-    <div class="flex flex-col items-center justify-center sm:flex sm:flex-row sm:items-center sm:justify-between">
+    <div
+      class="flex flex-col items-center justify-center sm:flex sm:flex-row sm:items-center sm:justify-between"
+    >
       <div class="md:flex-1 min-w-0 flex items-center gap-2">
         <main-icon class="h-10"></main-icon>
-        <h2 class="text-3xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
+        <h1
+          class="text-3xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate"
+        >
           RPG Dice Calculator
-        </h2>
+        </h1>
       </div>
       <div class="mt-4 flex md:mt-0 md:ml-4">
-        <button type="button"
-                class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500 print:hidden"
-                @click="restart"
+        <button
+          type="button"
+          class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500 print:hidden"
+          @click="restart"
         >
           Restart
         </button>
-        <button type="button"
-                class="ml-3 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-emerald-600 hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500 print:hidden"
-                @click="printOpen = true"
-                v-if="isDisplayStage"
+        <button
+          type="button"
+          class="ml-3 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-emerald-600 hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500 print:hidden"
+          @click="printOpen = true"
+          v-if="isDisplayStage"
         >
           Print
         </button>
       </div>
     </div>
-    <print-dialog :open="printOpen" @close="printOpen = false" @print="printChart"></print-dialog>
+    <print-dialog
+      :open="printOpen"
+      @close="printOpen = false"
+      @print="printChart"
+    ></print-dialog>
   </div>
 </template>
 
 <script lang="ts">
-import MainIcon from '@/components/dice-icons/MainIcon.vue'
-import {defineComponent, ref} from "vue";
+import MainIcon from "@/components/dice-icons/MainIcon.vue";
+import { defineComponent, ref } from "vue";
 import useDice from "@/store/dice";
 import useCalculations from "@/store/calculations";
 import useSteps from "@/store/steps";
@@ -39,33 +49,31 @@ export default defineComponent({
   name: "PageHeader",
   components: {
     PrintDialog,
-    MainIcon
+    MainIcon,
   },
   data() {
     return {
       printOpen: false,
-    }
+    };
   },
   setup() {
-    const {resetDice} = useDice();
-    const {resetCalc} = useCalculations();
-    const {resetStage, isDisplayStage} = useSteps();
+    const { resetDice } = useDice();
+    const { resetCalc } = useCalculations();
+    const { resetStage, isDisplayStage } = useSteps();
     const restart = () => {
       resetDice();
       resetCalc();
       resetStage();
-    }
-    return {restart, isDisplayStage};
+    };
+    return { restart, isDisplayStage };
   },
   methods: {
     printChart() {
       this.printOpen = false;
       setTimeout(() => window.print(), 500);
-    }
+    },
   },
 });
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
